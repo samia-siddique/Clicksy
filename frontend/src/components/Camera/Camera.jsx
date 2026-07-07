271ad366173211cea2aed77de3573936d457d3f9
Samia Siddique <samia.siddique34@example.com>
2026-07-08 01:27:12 +0500

    Add 3second photo timer

--- frontend/src/components/Camera/Camera.jsx
+++ frontend/src/components/Camera/Camera.jsx
@@ -6,26 +6,52 @@ const Camera = () => {
   const webcamRef = useRef(null);
   const [capturedImage, setCapturedImage] = useState([]);
   const navigate = useNavigate();
+  const [countDown, setCountDown] = useState(null);
 
   //Take photo three times
   useEffect(() => {
+    if (capturedImage.length === 0) return;
     if (capturedImage.length === 3) {
       navigate("/customize");
+    } else {
+      startCount();
     }
   }, [capturedImage, navigate]);
+
+  //Set 3sec CountDown Timer
+  const startCount = () => {
+    if (countDown !== null) return;
+    setCountDown(3);
+  };
+
+ useEffect(() => {
+  if (countDown === null) return;
+
+  if (countDown === 0) {
+    capture();
+    setCountDown(null);
+    return;
+  }
+
+  const timer = setTimeout(() => {
+    setCountDown((prev) => prev - 1);
+  }, 1000);
+
+  return () => clearTimeout(timer);
+}, [countDown]);
+
 
   //Take Photo
   const capture = () => {
     const imgScreenShot = webcamRef.current.getScreenshot();
-    console.log(imgScreenShot);
-
     setCapturedImage((prev) => [...prev, imgScreenShot]);
   };
 
   return (
     <div>
+      {countDown !== null && <h1>{countDown}</h1>}
       <Webcam screenshotFormat="image/png" ref={webcamRef} />
-      <button onClick={capture}>cheez</button>
+      <button onClick={startCount}>cheez</button>
 
       {capturedImage.map((image, index) => (
         <img src={image} key={index} alt={`captured ${index + 1}`} />
