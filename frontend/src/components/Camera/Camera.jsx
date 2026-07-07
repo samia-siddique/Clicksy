8303362cdb80e30781985b7b6a26c39db43330c7
Samia Siddique <samia.siddique34@example.com>
2026-07-07 21:40:37 +0500

    Add take photo 3 times feauture

--- frontend/src/components/Camera/Camera.jsx
+++ frontend/src/components/Camera/Camera.jsx
@@ -1,15 +1,25 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Webcam from "react-webcam";
 
 const Camera = () => {
   const webcamRef = useRef(null);
   const [capturedImage, setCapturedImage] = useState([]);
+  const navigate = useNavigate();
 
+  //Take photo three times
+  useEffect(() => {
+    if (capturedImage.length === 3) {
+      navigate("/customize");
+    }
+  }, [capturedImage, navigate]);
+
+  //Take Photo
   const capture = () => {
     const imgScreenShot = webcamRef.current.getScreenshot();
     console.log(imgScreenShot);
 
-    setCapturedImage(imgScreenShot);
+    setCapturedImage((prev) => [...prev, imgScreenShot]);
   };
 
   return (
@@ -17,7 +27,9 @@ const Camera = () => {
       <Webcam screenshotFormat="image/png" ref={webcamRef} />
       <button onClick={capture}>cheez</button>
 
-      {capturedImage && <img src={capturedImage} alt="" />}
+      {capturedImage.map((image, index) => (
+        <img src={image} key={index} alt={`captured ${index + 1}`} />
+      ))}
     </div>
   );
 };
