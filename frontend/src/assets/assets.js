cd61961055430dd50c3fd7373773142ba3be6a9a
Samia Siddique <samia.siddique34@example.com>
2026-07-09 00:36:48 +0500

    Add the Home Page CSS

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,15 @@
+import camera from "./camera.png";
+import globe from "./globe.png";
+import receipt from "./receipt.png";
+import star from "./star.png";
+import tv from "./tv.png";
+
+const assets = {
+  camera: camera,
+  globe: globe,
+  receipt: receipt,
+  star: star,
+  tv: tv,
+};
+
+export default assets;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import "./Home.css";
 import { useNavigate } from "react-router-dom";
+import assets from "../../assets/assets";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className="home">
       <h1>Clicksy</h1>
-      {/* <img src="" alt="" /> */}
-      <button onClick={() => navigate("/selectPhoto")}>Start</button>
+      <p>Pose, Click, Repeat</p>
+
+      <div className="home-images">
+        <img src={assets.camera} alt="" />
+      </div>
+
+      <button class="start-btn" onClick={() => navigate("/selectPhoto")}>
+        Start
+      </button>
     </div>
   );
 };
